92b63fda235acf0dd671d3156441af5e3d96bacc
Daniel Mateu <danielmateu86@gmail.com>
2023-10-13 13:54:54 +0200

    Landing page + autenticación completada

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,18 +1,53 @@
 "use client"
+import { Spinner } from "@/components/Spinner"
 import { Button } from "@/components/ui/button"
+import { SignInButton } from "@clerk/clerk-react"
+import { useConvexAuth } from "convex/react"
 import { ArrowRightIcon } from "lucide-react"
+import Link from "next/link"
 
 export const Heading = () => {
+
+    const { isAuthenticated, isLoading } = useConvexAuth()
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-semibold">Tus ideas, documentos y planes unificados. Bienvenid@ a <span className="underline">Nukotion</span></h1>
 
             <h3 className='text-base sm:text-xl md:text-2xl font-medium'>Nukotion es el espacio de trabajo donde las cosas pasan 🐶</h3>
 
-            <Button className='group'>
-                Entra a Nukotion
-                <ArrowRightIcon className="h-4 w-4 ml-2 group-hover:translate-x-2 transition" />
-            </Button>
+            {
+                isLoading && (
+                    <div className="w-full flex items-center justify-center">
+                        <Spinner
+                            size={'lg'}
+                        />
+                    </div>
+                )
+            }
+
+            {
+                isAuthenticated && !isLoading && (
+                    <Button className='group' asChild>
+                        <Link href={'/documents'}>
+                            Entra a Nukotion
+                            <ArrowRightIcon className="h-4 w-4 ml-2 group-hover:translate-x-2 transition" />
+                        </Link>
+                    </Button>
+                )
+            }
+
+            {
+                !isAuthenticated && !isLoading && (
+                    <SignInButton mode="modal">
+                        <Button className="group">
+                            Registrate gratis
+                            <ArrowRightIcon className="h-4 w-4 ml-2 group-hover:translate-x-2 transition" />
+                        </Button>
+
+                    </SignInButton>
+                )
+            }
+
 
         </div>
     )
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,9 +4,16 @@ import { Logo } from "./Logo"
 import { useScrollTop } from "@/hooks/useScrollTop"
 import { cn } from '../../../lib/utils';
 import { ModeToggle } from "@/components/ModeToggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/Spinner";
+import Link from "next/link";
+
 
 export const Navbar = () => {
 
+    const { isAuthenticated, isLoading } = useConvexAuth()
     const scrolled = useScrollTop()
 
     return (
@@ -16,6 +23,41 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {
+                    isLoading && (
+                        <Spinner />
+                    )
+                }
+                {
+                    !isAuthenticated && !isLoading && (
+                        <>
+                            <SignInButton mode="modal" >
+                                <Button variant={'ghost'} size={'sm'}>
+                                    Iniciar sesión
+                                </Button>
+                            </SignInButton>
+                            <SignInButton mode="modal" >
+                                <Button size={'sm'}>
+                                    Registrate gratis
+                                </Button>
+                            </SignInButton>
+                        </>
+                    )
+                }
+                {
+                    isAuthenticated && !isLoading && (
+                        <>
+                            <Button variant={'ghost'} size={'sm'} asChild>
+                                <Link href={'/documents'}>
+                                    Entra a Nukotion
+                                </Link>
+                            </Button>
+                            <UserButton
+                                afterSignOutUrl="/"
+                            />
+                        </>
+                    )
+                }
                 <ModeToggle />
             </div>
         </div>
