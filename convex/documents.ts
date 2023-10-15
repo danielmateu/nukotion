2296fa0f52a46a8d7b8d0eb15cedb44de8478ced
Daniel Mateu <danielmateu86@gmail.com>
2023-10-15 10:41:09 +0200

    definicion de funcion archive que de forma recursiva archiva el documento y sus hijos

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,63 @@
 import { v as v } from "convex/values"
 import { mutation, query } from "./_generated/server"
+import { Id } from "./_generated/dataModel";
+
+export const archive = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Documento no encontrado");
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("No tienes permisos para editar este documento");
+        }
+
+        /**
+         * The recursiveArchive function archives a document and all its children documents.
+         * @param documentId - The documentId parameter is the unique identifier of a document that you
+         * want to recursively archive.
+         */
+        const recursiveArchive = async (documentId: Id<"documents">) => {
+            const children = await ctx.db.query("documents").withIndex("by_user_parent", (q) => (
+                q
+                    .eq("userId", userId)
+                    .eq("parentDocument", documentId)
+            ))
+                .collect();
+
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true
+                })
+
+                await recursiveArchive(child._id);
+            }
+        }
+
+        /* The code `const document = await ctx.db.patch(args.id, { isArchived: true })` is updating
+        the `isArchived` property of a document with the specified `args.id` to `true`. It is using
+        the `ctx.db.patch` method to update the document in the database. The updated document is
+        then stored in the `document` variable. */
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true
+        })
+
+        recursiveArchive(args.id);
+
+        return document;
+    }
+});
 
 export const getSidebar = query({
     args: {
