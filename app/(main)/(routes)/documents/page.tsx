8ce24b113384b59dffbc16479b609802a1910748
Daniel Mateu <danielmateu86@gmail.com>
2023-11-15 11:39:21 +0100

    Item, redirection done

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -6,26 +6,25 @@ import { useUser } from "@clerk/clerk-react"
 import { useMutation } from "convex/react"
 import { PlusCircle } from "lucide-react"
 import Image from "next/image"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { toast } from "sonner"
-
 
 const DocumentsPage = () => {
 
     const { user } = useUser()
     const create = useMutation(api.documents.create);
-    // const router = useRouter();
+    const router = useRouter();
 
-    // const onCreate = () => {
-    //     const promise = create({ title: "Sin título" })
-    //         .then((documentId) => router.push(`/documents/${documentId}`))
+    const onCreate = () => {
+        const promise = create({ title: "Sin título" })
+            .then((documentId) => router.push(`/documents/${documentId}`))
 
-    //     toast.promise(promise, {
-    //         loading: "Creando una nota nueva...",
-    //         success: "Nota creada! 😊",
-    //         error: "Ha ocurrido algún problema al crear la nota 😱"
-    //     })
-    // }
+        toast.promise(promise, {
+            loading: "Creando una nota nueva...",
+            success: "Nota creada! 😊",
+            error: "Ha ocurrido algún problema al crear la nota 😱"
+        })
+    }
 
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -45,10 +44,10 @@ const DocumentsPage = () => {
             />
             <h2 className="text-lg font-medium"
             >Bienvenid@ al Nukotion de {user?.firstName}</h2>
-            {/* <Button onClick={onCreate}>
+            <Button onClick={onCreate}>
                 <PlusCircle className="w-4 h-4 mr-2" />
                 Crea una nota
-            </Button> */}
+            </Button>
         </div>
     )
 }
--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -75,7 +75,7 @@ export const Item = ({
                     onExpand?.();
                 }
 
-                // router.push(`/documents/${documentId}`)
+                router.push(`/documents/${documentId}`)
             })
 
         toast.promise(promise, {
